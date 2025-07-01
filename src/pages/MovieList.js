8088345f9b5bfcfc2c9b8bca29a4b7f43f0f89af
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-07-01 21:21:51 +0530

    card added

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,17 @@
+import {Card} from "../components"
+
+
 export const MovieList = () => {
   return (
-    <main>MovieList</main>
+    <main>
+
+      <section className="max-w-7xl mx-auto py-7">
+        <div className="flex justify-start flex-wrap">
+              <Card/>
+              <Card/>
+
+        </div>
+      </section>
+    </main>
   )
 }
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -4,7 +4,8 @@ import { MovieDetail,MovieList,Search,PageNotFound } from "../pages/index"
 export const  AllRoutes= () => {
   return (
     <>
-    <Routes>
+    <div className="dark:bg-slate-800">
+      <Routes>
         <Route path="" element={<MovieList/>} ></Route>
         <Route path="movie/:id" element={<MovieDetail/>} ></Route>
         <Route path="movies/popular" element={<MovieDetail/>} ></Route>
@@ -12,7 +13,8 @@ export const  AllRoutes= () => {
         <Route path="movies/upcoming" element={<MovieDetail/>} ></Route>
         <Route path="Search" element={<Search/>} ></Route>
         <Route path="*" element={<PageNotFound/>} ></Route>
-    </Routes>
+      </Routes>
+    </div>
     </>
   )
 }
