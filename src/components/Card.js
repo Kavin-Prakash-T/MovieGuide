d0e38cb5b0dae610ef66da522cde522ed02f074b
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-07-03 21:06:41 +0530

    scroll feature

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ const image = poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : B
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 m-3">
       <Link to={`/movie/${id}`}>
-        <img className="rounded-t-lg" src={image} alt="" />
+        <img className="rounded-t-lg" src={image} alt={original_title} />
       </Link>
       <div className="p-5">
         <Link to={`/movie/${id}`}>
