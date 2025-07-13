fe239e47ac3f417b9425530502cb1099b3bff2b0
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-07-13 17:23:08 +0530

    updated

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -6,7 +6,7 @@ export const AllRoutes = () => {
     <>
       <div className="dark:bg-slate-800">
         <Routes>
-          <Route path="" element={<MovieList apiPath="movie/now_playing" title="Home" />} ></Route>
+          <Route path="/" element={<MovieList apiPath="movie/now_playing" title="Home" />} ></Route>
           <Route path="movie/:id" element={<MovieDetail />} ></Route>
           <Route path="movies/popular" element={<MovieList apiPath="movie/now_playing" title="Popular" />} ></Route>
           <Route path="movies/top" element={<MovieList apiPath="movie/top_rated" title="Top Rated" />} ></Route>
